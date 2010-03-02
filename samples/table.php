824de8e390cc819ea3e93d8569edf7b0b80dde97
asual <rostislav@asual.com>
2010-03-02 15:02:04 +0200

    Adds a new Ajax sample.

---
+++ samples/table.php
@@ -0,0 +1,131 @@
+<?php 
+    function ts() {
+        echo($_GET['ts']);
+    }
+?>
+<table class="sample4">
+    <thead>
+        <tr>
+            <th>Column 1 <?php ts(); ?></th>
+            <th style="width: 200px;">Column 2 <?php ts(); ?></th>
+            <th style="width: 200px;">Column 3 <?php ts(); ?></th>
+        </tr>
+    </thead>
+    <tfoot>
+        <tr>
+            <td colspan="3"><a href="#">Reload table</a></td>
+        </tr>
+    </tfoot>
+    <tbody>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr class="even">
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+        <tr>
+            <td>Column 1 <?php ts(); ?></td>
+            <td>Column 2 <?php ts(); ?></td>
+            <td>Column 3 <?php ts(); ?></td>
+        </tr>
+    </tbody>
+</table>
